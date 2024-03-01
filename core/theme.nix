92138af62471fabf37754d71196c6353d2ac9cad
zackartz <zackmyers@lavabit.com>
2024-02-29 23:57:43 -0500

    62 current  2024-02-29 23:57:39  24.05.20240228.9099616   6.6.18                          *

--- core/theme.nix
+++ core/theme.nix
@@ -2,21 +2,21 @@
   x = c: "#${c}";
 
   colors = rec {
-    "base" = "f191724";
-    "surface" = "f1f1d2e";
-    "overlay" = "f26233a";
-    "muted" = "f6e6a86";
-    "subtle" = "f6e6a86";
-    "text" = "ffe0def4";
-    "love" = "ffeb6f92";
-    "gold" = "fff6c177";
-    "rose" = "ffebbcba";
-    "pine" = "ff31748f";
-    "foam" = "ff9ccfd8";
-    "iris" = "ffc4a7e7";
-    "highlightlow" = "ff21202e";
-    "highlightmed" = "ff403d52";
-    "highlighthigh" = "ff524f67";
+    "base" = "191724";
+    "surface" = "1f1d2e";
+    "overlay" = "26233a";
+    "muted" = "6e6a86";
+    "subtle" = "6e6a86";
+    "text" = "e0def4";
+    "love" = "eb6f92";
+    "gold" = "f6c177";
+    "rose" = "ebbcba";
+    "pine" = "31748f";
+    "foam" = "9ccfd8";
+    "iris" = "c4a7e7";
+    "highlightlow" = "21202e";
+    "highlightmed" = "403d52";
+    "highlighthigh" = "524f67";
   };
 
   wallpaper = ./wall.png;
