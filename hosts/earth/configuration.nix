2d83a4ff135198e7776206cbdb82cf4f46830149
zackartz <zackmyers@lavabit.com>
2024-02-29 23:16:53 -0500

    55 current  2024-02-29 23:05:52  24.05.20240228.9099616   6.6.18                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -19,11 +19,6 @@
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "earth"; # Define your hostname.
-  # networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
-
-  # Configure network proxy if necessary
-  # networking.proxy.default = "http://user:password@proxy:port/";
-  # networking.proxy.noProxy = "127.0.0.1,localhost,internal.domain";
 
   # Enable networking
   networking.networkmanager.enable = true;
