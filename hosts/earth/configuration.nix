1bdaa403944df85554b79fe6e25cae6beac302d0
zackartz <zackmyers@lavabit.com>
2024-02-29 23:05:56 -0500

    55 current  2024-02-29 23:05:52  24.05.20240228.9099616   6.6.18                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -31,6 +31,8 @@
   # Set your time zone.
   time.timeZone = "America/New_York";
 
+  boot.supportedFilesystems = ["ntfs"];
+
   # Select internationalisation properties.
   i18n.defaultLocale = "en_US.UTF-8";
 
