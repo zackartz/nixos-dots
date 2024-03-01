dbd3877d98e4a2a6186b37c42fe126528eab5038
zackartz <zackmyers@lavabit.com>
2024-03-01 11:02:53 -0500

    79 current  2024-03-01 11:02:48  24.05.20240228.9099616   6.6.18                          *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -63,6 +63,7 @@
     pkgs.swaybg
     pkgs.btop
     pkgs.zoom-us
+    pkgs.pavucontrol
 
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
