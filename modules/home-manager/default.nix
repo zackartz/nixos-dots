b9bd6173300f1b449944e3712562ca63e3b33d24
zackartz <zackmyers@lavabit.com>
2024-03-06 11:46:02 -0500

    143 current  2024-03-06 11:35:15  24.05.20240303.b8697e5   6.6.19                          *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -36,6 +36,12 @@
     options = "--delete-older-than 30d";
   };
 
+  xdg.mimeApps.defaultApplications = {
+    "text/html" = "firefox";
+    "x-scheme-handler/http" = "firefox";
+    "x-scheme-handler/https" = "firefox";
+  };
+
   # This value determines the Home Manager release that your configuration is
   # compatible with. This helps avoid breakage when a new Home Manager release
   # introduces backwards incompatible changes.
