5fd1c829c573a1139f6d1ce71afe65d45c086874
zackartz <zackmyers@lavabit.com>
2024-03-13 14:07:02 -0400

    185 current  2024-03-13 14:06:54  24.05.20240309.3030f18   6.7.9-zen1                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -38,10 +38,10 @@
   # Load nvidia driver for Xorg and Wayland
   services.xserver.videoDrivers = ["nvidia"]; # or "nvidiaLegacy470 etc.
 
-  services.openssh = {
-    enable = true;
-    PasswordAuthentication = true;
-  };
+  # services.openssh = {
+  #   enable = true;
+  #   PasswordAuthentication = true;
+  # };
 
   environment.systemPackages = with pkgs; [
     alvr
