f5f00b5d63de73be50169ad31c22dd164d7a9500
zackartz <zackmyers@lavabit.com>
2024-03-13 12:12:09 -0400

    167 current  2024-03-13 12:12:01  24.05.20240309.3030f18   6.7.9-zen1                          *

--- modules/home-manager/vim.nix
+++ modules/home-manager/vim.nix
@@ -276,7 +276,18 @@
     extraPlugins = with pkgs.vimPlugins; [
       {
         plugin = dressing-nvim;
-        config = ''lua require('dressing').setup()'';
+        config = ''          lua function config()
+                    local theme = require("telescope.themes").get_dropdown()
+
+                    theme.layout_config = { width = 60, height = 17 }
+
+                    require("dressing").setup({
+                      input = { enabled = false },
+                      select = { backend = { "telescope" }, telescope = theme }
+                    })
+                    end
+                    config()
+        '';
       }
       {
         plugin = overseer-nvim;
