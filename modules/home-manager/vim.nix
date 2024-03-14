ab55399e8c63aa2e2135fb466302f23a337757fd
zackartz <zackmyers@lavabit.com>
2024-03-14 13:58:53 -0400

    200 current  2024-03-14 13:58:44  24.05.20240312.0ad13a6   6.8.0-zen1                          *

--- modules/home-manager/vim.nix
+++ modules/home-manager/vim.nix
@@ -1,8 +1,4 @@
-{
-  pkgs,
-  inputs,
-  ...
-}: {
+{pkgs, ...}: {
   config = {
     enable = true;
 
@@ -414,10 +410,13 @@
         snippet = {expand = "luasnip";};
         mappingPresets = ["insert"];
         mapping = {
-          "<CR>" = "cmp.mapping.confirm({ select = true })";
-          "<C-space>" = "cmp.mapping.complete()";
-          "<C-b>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-Space>" = "cmp.mapping.complete()";
+          "<C-d>" = "cmp.mapping.scroll_docs(-4)";
+          "<C-e>" = "cmp.mapping.close()";
           "<C-f>" = "cmp.mapping.scroll_docs(4)";
+          "<CR>" = "cmp.mapping.confirm({ select = true })";
+          "<S-Tab>" = "cmp.mapping(cmp.mapping.select_prev_item(), {'i', 's'})";
+          "<Tab>" = "cmp.mapping(cmp.mapping.select_next_item(), {'i', 's'})";
         };
       };
     };
