e1f45c6bac70601246cef9c612e1ee4ab26c3242
zackartz <zackmyers@lavabit.com>
2024-03-20 12:19:21 -0400

    242 current  2024-03-20 12:19:12  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/lsp/mason-lspconfig.lua
+++ modules/home-manager/vim/config/lua/plugins/lsp/mason-lspconfig.lua
@@ -1,17 +1,18 @@
-return {
-  "williamboman/mason-lspconfig.nvim",
-  dependencies = { "neovim/nvim-lspconfig" },
-  event = { "BufReadPost", "BufNewFile" },
-  opts = {
-    handlers = require("lsp.handlers"),
-    ensure_installed = {
-      "rust_analyzer",
-      "tsserver",
-      "lua_ls",
-      "bashls",
-      "cssls",
-      "tailwindcss",
-      "jsonls",
-    },
-  },
-}
+-- return {
+--   "williamboman/mason-lspconfig.nvim",
+--   dependencies = { "neovim/nvim-lspconfig" },
+--   event = { "BufReadPost", "BufNewFile" },
+--   opts = {
+--     handlers = require("lsp.handlers"),
+--     ensure_installed = {
+--       "rust_analyzer",
+--       "tsserver",
+--       "lua_ls",
+--       "bashls",
+--       "cssls",
+--       "tailwindcss",
+--       "jsonls",
+--     },
+--   },
+-- }
+return {}
--- modules/home-manager/vim/config/lua/plugins/lsp/mason.lua
+++ modules/home-manager/vim/config/lua/plugins/lsp/mason.lua
@@ -1,16 +1,17 @@
-local icons = require("utils.icons").icons
-
-return {
-  "williamboman/mason.nvim",
-  build = ":MasonUpdate",
-  opts = {
-    ui = {
-      border = "rounded",
-      icons = {
-        package_installed = icons.Check,
-        package_pending = icons.Dots,
-        package_uninstalled = icons.Install,
-      },
-    },
-  },
-}
+-- local icons = require("utils.icons").icons
+--
+-- return {
+--   "williamboman/mason.nvim",
+--   build = ":MasonUpdate",
+--   opts = {
+--     ui = {
+--       border = "rounded",
+--       icons = {
+--         package_installed = icons.Check,
+--         package_pending = icons.Dots,
+--         package_uninstalled = icons.Install,
+--       },
+--     },
+--   },
+-- }
+return {}
