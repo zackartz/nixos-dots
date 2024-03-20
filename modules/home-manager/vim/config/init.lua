61886c5f5ecb7d76903c48ff69b8b54218bda87d
zackartz <zackmyers@lavabit.com>
2024-03-20 16:42:43 -0400

    257 current  2024-03-20 16:42:33  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/init.lua
+++ modules/home-manager/vim/config/init.lua
@@ -3,20 +3,24 @@ local configurer = require("utils.configurer")
 local opts = {}
 
 if vim.g.vscode then
-  -- VSCode Neovim
-  opts.spec = "vscode.plugins"
-  opts.options = require("vscode.options")
-  opts.keymaps = require("vscode.keymaps")
+	-- VSCode Neovim
+	opts.spec = "vscode.plugins"
+	opts.options = require("vscode.options")
+	opts.keymaps = require("vscode.keymaps")
 else
-  -- Normal Neovim
-  opts.spec = "plugins"
-  opts.options = require("core.options")
-  opts.keymaps = require("core.keymaps")
-  opts.autocmd = require("core.autocmd")
-  opts.signs = require("core.signs")
+	-- Normal Neovim
+	opts.spec = "plugins"
+	opts.options = require("core.options")
+	opts.keymaps = require("core.keymaps")
+	opts.autocmd = require("core.autocmd")
+	opts.signs = require("core.signs")
 end
 
 configurer.setup(opts)
+
+-- Neovide config
+vim.o.guifont = "Iosevka Nerd Font Mono:h14"
+vim.g.neovide_transparency = 0.75
 
 -- vim.lsp.log.set_level(vim.lsp.log_levels.INFO)
 vim.filetype.add(filetypes)
