fbd9a25dc8d4554092c2732fcfef331b482c2ec0
zackartz <zackmyers@lavabit.com>
2024-03-20 12:13:45 -0400

    240 current  2024-03-20 12:13:36  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/ui/dropbar.lua
+++ modules/home-manager/vim/config/lua/plugins/ui/dropbar.lua
@@ -1,18 +1,18 @@
-return {
-  {
-    "Bekaboo/dropbar.nvim",
-    event = { "BufRead", "BufNewFile" },
-    opts = {
-      icons = {
-        enable = true,
-        kinds = {
-          use_devicons = false,
-          symbols = {
-            File = "",
-            Folder = "",
-          },
-        },
-      },
-    },
-  },
-}
+-- return {
+--   {
+--     "Bekaboo/dropbar.nvim",
+--     event = { "BufRead", "BufNewFile" },
+--     opts = {
+--       icons = {
+--         enable = true,
+--         kinds = {
+--           use_devicons = false,
+--           symbols = {
+--             File = "",
+--             Folder = "",
+--           },
+--         },
+--       },
+--     },
+--   },
+-- }
