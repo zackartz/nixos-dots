272257d21be81fb9905b463e30f6dee7cc1cae3f
zackartz <zackmyers@lavabit.com>
2024-03-20 12:23:27 -0400

    244 current  2024-03-20 12:23:17  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/editor/nvim-dap.lua
+++ modules/home-manager/vim/config/lua/plugins/editor/nvim-dap.lua
@@ -5,18 +5,6 @@ return {
   },
   cmd = { "DapContinue", "DapToggleBreakpoint" },
   dependencies = {
-    {
-      "jay-babu/mason-nvim-dap.nvim",
-      dependencies = { "nvim-dap" },
-      cmd = { "DapInstall", "DapUninstall" },
-      opts = {
-        handlers = {},
-        ensure_installed = {
-          "codelldb",
-          "node-debug2-adapter",
-        },
-      },
-    },
     { "rcarriga/nvim-dap-ui", opts = {} },
 { "nvim-neotest/nvim-nio" },
     {
