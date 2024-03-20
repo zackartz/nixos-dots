beb60ea1ec62fbd2eeeae69261052c4ac41532bd
zackartz <zackmyers@lavabit.com>
2024-03-20 12:47:01 -0400

    248 current  2024-03-20 12:46:51  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/lua/lsp/config.lua
+++ modules/home-manager/vim/config/lua/lsp/config.lua
@@ -1,27 +1,29 @@
 local M = {}
 
 M.set_autocmd = function(client, bufnr)
-  local capability_map = require("lsp.autocmd")
+	local capability_map = require("lsp.autocmd")
 
-  for capability, map in pairs(capability_map) do
-    if client.supports_method(capability) then
-      vim.api.nvim_clear_autocmds({ group = map.opts.group, buffer = bufnr })
-      map.opts.buffer = bufnr
-      vim.api.nvim_create_autocmd(map.event, map.opts)
-    end
-  end
+	for capability, map in pairs(capability_map) do
+		if client.supports_method(capability) then
+			vim.api.nvim_clear_autocmds({ group = map.opts.group, buffer = bufnr })
+			map.opts.buffer = bufnr
+			vim.api.nvim_create_autocmd(map.event, map.opts)
+		end
+	end
 end
 
 M.set_keymaps = function(client, bufnr)
-  local capability_map = require("lsp.keymaps")
+	local capability_map = require("lsp.keymaps")
 
-  for capability, maps in pairs(capability_map) do
-    if client.supports_method(capability) then
-      for key, map in pairs(maps) do
-        vim.keymap.set("n", key, map[1], { desc = map[2], buffer = bufnr })
-      end
-    end
-  end
+	for capability, maps in pairs(capability_map) do
+		if client.supports_method(capability) then
+			for key, map in pairs(maps) do
+				vim.keymap.set("n", key, map[1], { desc = map[2], buffer = bufnr })
+			end
+		end
+	end
 end
+
+require("lspconfig").tsserver.setup({})
 
 return M
