39fc9422c2d9433a03b6d44539e8511d11cf7c10
zackartz <zackmyers@lavabit.com>
2024-03-20 12:30:13 -0400

    245 current  2024-03-20 12:30:04  24.05.20240319.b06025f   6.8.0-zen1                          *

---
+++ modules/home-manager/vim/config/lua/plugins/editor/overseer.lua
@@ -0,0 +1,14 @@
+return {
+  'stevearc/overseer.nvim',
+  opts = {
+task_list = {
+  direction = "bottom",
+},
+  },
+  dependencies = {
+    {
+  'stevearc/dressing.nvim',
+  opts = {},
+}
+  },
+}
