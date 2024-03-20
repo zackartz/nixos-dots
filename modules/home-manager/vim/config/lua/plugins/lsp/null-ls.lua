f049c4d40b2b146199fcfa83b0a56b7d09a221c3
zackartz <zackmyers@lavabit.com>
2024-03-20 12:50:46 -0400

    249 current  2024-03-20 12:50:36  24.05.20240319.b06025f   6.8.0-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/lsp/null-ls.lua
+++ modules/home-manager/vim/config/lua/plugins/lsp/null-ls.lua
@@ -1,25 +1,25 @@
 return {
-  "jose-elias-alvarez/null-ls.nvim",
-  event = { "BufReadPost", "BufNewFile" },
-  config = function()
-    local null_ls = require("null-ls")
+	"jose-elias-alvarez/null-ls.nvim",
+	event = { "BufReadPost", "BufNewFile" },
+	config = function()
+		local null_ls = require("null-ls")
 
-    null_ls.setup({
-      debug = false,
-      sources = {
-        null_ls.builtins.formatting.prettier,
-        null_ls.builtins.formatting.stylua,
-        null_ls.builtins.formatting.rustfmt.with({
-          extra_args = { "--edition=2021" },
-        }),
-        null_ls.builtins.formatting.shfmt.with({
-          filetypes = { "sh", "zsh" },
-          extra_args = { "--indent-type Spaces" },
-        }),
-        null_ls.builtins.formatting.clang_format.with({
-          extra_args = { "-style={IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty}" },
-        }),
-      },
-    })
-  end,
+		null_ls.setup({
+			debug = false,
+			sources = {
+				null_ls.builtins.formatting.prettierd,
+				null_ls.builtins.formatting.stylua,
+				null_ls.builtins.formatting.rustfmt.with({
+					extra_args = { "--edition=2021" },
+				}),
+				null_ls.builtins.formatting.shfmt.with({
+					filetypes = { "sh", "zsh" },
+					extra_args = { "--indent-type Spaces" },
+				}),
+				null_ls.builtins.formatting.clang_format.with({
+					extra_args = { "-style={IndentWidth: 4, AllowShortFunctionsOnASingleLine: Empty}" },
+				}),
+			},
+		})
+	end,
 }
