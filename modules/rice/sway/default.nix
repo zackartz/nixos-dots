deedd53c276b17d1e392d7ef660c6083dc08913f
zackartz <zackmyers@lavabit.com>
2024-04-03 13:23:14 -0400

    305 current  2024-04-03 13:23:05  24.05.20240402.43f9562  6.8.2-zen2                          *

--- modules/rice/sway/default.nix
+++ modules/rice/sway/default.nix
@@ -4,6 +4,12 @@
     config = rec {
       terminal = "kitty";
       startup = [{command = "firefox";}];
+
+      output = {
+        DP-1 = {
+          mode = "2560x1440@239.972Hz";
+        };
+      };
     };
     extraOptions = ["--unsupported-gpu"];
   };
