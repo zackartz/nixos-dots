77135ab1c9e6b7035eb7a017884abe4dfa6261d2
zackartz <zackmyers@lavabit.com>
2024-04-03 00:29:27 -0400

    300 current  2024-04-03 00:29:13  24.05.20240402.43f9562  6.8.2-zen2                          *

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "Nixos config flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
