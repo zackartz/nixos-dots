eca95b520ca4ebb32ea3717a0e1233a4923ebbf9
zackartz <zackmyers@lavabit.com>
2024-04-13 10:53:21 -0400

    334 current  2024-04-13 10:53:12  24.05.20240411.681d4a8  6.8.4-zen1                          *

--- modules/home-manager/default.nix
+++ modules/home-manager/default.nix
@@ -108,6 +108,8 @@
     pkgs.devenv
     pkgs.ghidra
 
+    pkgs.zed-editor
+
     # # You can also create simple shell scripts directly inside your
     # # configuration. For example, this adds a command 'my-hello' to your
     # # environment:
