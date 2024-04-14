7f0f1d0dcc2835db3df7552b1b01aafbae21ec85
zackartz <zackmyers@lavabit.com>
2024-04-14 16:35:29 -0400

    342 current  2024-04-14 16:35:21  24.05.20240414.bc59f72  6.8.4-zen1                          *

--- modules/shell/tmux.nix
+++ modules/shell/tmux.nix
@@ -23,9 +23,23 @@ in {
       }
       tmuxPlugins.sensible
       tmuxPlugins.vim-tmux-navigator
+      tmuxPlugins.yank
     ];
     extraConfig = ''
       set-option -sa terminal-overrides ",xterm*:Tc"
+      set -g mouse on
+
+      set -g base-index 1
+      set -g pane-base-index 1
+      set-window-option -g pane-base-index 1
+      set-option -g renumber-windows on
+
+      bind-key -T copy-mode-vi v send-keys -X begin-selection
+      bind-key -T copy-mode-vi C-v send-keys -X rectangle-toggle
+      bind-key -T copy-mode-vi y send-keys -X copy-selection-and-cancel
+
+      bind '"' split-window -v -c "#{pane_current_path}"
+      bind % split-window -v -c "#{pane_current_path}"
 
       unbind C-b
       set -g prefix C-Space
