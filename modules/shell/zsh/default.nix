e61af62f456b766bc5ee80fd972ad1aaf7f42ee4
zackartz <zackmyers@lavabit.com>
2024-04-16 13:19:02 -0400

    346 current  2024-04-16 13:18:56  24.05.20240416.c04952a  6.8.4-zen1                          *

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -32,14 +32,14 @@
     plugins = [
       {
         name = "zsh-nix-shell";
-        file = "nix-shell.plugin.zsh";
-        src = pkgs.fetchFromGitHub {
-          owner = "chisui";
-          repo = "zsh-nix-shell";
-          rev = "v0.7.0";
-          sha256 = "149zh2rm59blr2q458a5irkfh82y3dwdich60s9670kl3cl5h2m1";
-        };
+        src = pkgs.zsh-nix-shell;
       }
+      # zsh-history
+      # zsh-fzf-tab
+      # zsh-f-sy-h
+      # zsh-autocomplete
+      # zsh-you-should-use
+      # zsh-navigation-tools
     ];
   };
 }
