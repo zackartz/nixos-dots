76e98d422e12c6b8ce7455ce6d5f5130f5d840ad
zackartz <zackmyers@lavabit.com>
2024-04-16 14:19:20 -0400

    357 current  2024-04-16 14:19:12  24.05.20240416.c04952a  6.8.4-zen1                          *

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -44,8 +44,8 @@ in {
     in ''
       ${plugins}
 
-      bindkey "^[[1;3C" forward-word
-      bindkey "^[[1;3D" backward-word
+      bindkey "^[[1;5C" forward-word
+      bindkey "^[[1;5D" backward-word
     '';
 
     plugins = [
