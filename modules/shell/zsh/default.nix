d4036f288d7157a4ee0d3bd8a9a0514e8f2aa95c
zackartz <zackmyers@lavabit.com>
2024-04-16 14:15:12 -0400

    356 current  2024-04-16 14:15:05  24.05.20240416.c04952a  6.8.4-zen1                          *

--- modules/shell/zsh/default.nix
+++ modules/shell/zsh/default.nix
@@ -15,6 +15,7 @@ in {
       ZSH_AUTOSUGGEST_USE_ASYNC = "true";
       SSH_AUTH_SOCK = "/run/user/1000/keyring/ssh";
     };
+    autosuggestion.enable = true;
     history = {
       save = 2137;
       size = 2137;
@@ -29,7 +30,7 @@ in {
         "${zsh-history}/share/zsh/init.zsh"
         "${zsh-fzf-tab}/share/fzf-tab/fzf-tab.plugin.zsh"
         "${zsh-f-sy-h}/share/zsh/site-functions/F-Sy-H.plugin.zsh"
-        # "${zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
+        "${zsh-autocomplete}/share/zsh-autocomplete/zsh-autocomplete.plugin.zsh"
         "${zsh-you-should-use}/share/zsh/plugins/you-should-use/you-should-use.plugin.zsh"
         "${zsh-navigation-tools}/share/zsh/site-functions/zsh-navigation-tools.plugin.zsh"
       ];
