e3336c29577fadab23ebe6c7b8bcf3a6dc8ddea5
zackartz <zackmyers@lavabit.com>
2024-04-17 16:30:15 -0400

    378 current  2024-04-17 16:30:07  24.05.20240416.5672bc9  6.8.4-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/ui/catppuccin.lua
+++ modules/home-manager/vim/config/lua/plugins/ui/catppuccin.lua
@@ -1,8 +1,17 @@
 return {
 	"catppuccin/nvim",
 	name = "catppuccin",
+	init = function()
+		vim.cmd.colorscheme("catppuccin")
+	end,
 	opts = {
 		transparent_background = true,
+		integrations = {
+			cmp = true,
+			noice = true,
+			treesitter = true,
+			neotree = true,
+		},
 	},
 	priority = 1000,
 }
