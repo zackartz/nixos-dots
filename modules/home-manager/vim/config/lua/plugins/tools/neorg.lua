96f3c61b381f97a66a4fceaee3a958cf045b4368
zackartz <zackmyers@lavabit.com>
2024-04-19 00:51:13 -0400

    421 current  2024-04-19 00:51:12  24.05.20240418.bc279bb  6.8.6-zen1                          nvidiaProduction nvidiaStable nvidiaVulkanBeta

--- modules/home-manager/vim/config/lua/plugins/tools/neorg.lua
+++ modules/home-manager/vim/config/lua/plugins/tools/neorg.lua
@@ -1,4 +1,23 @@
 return {
 	{ "vhyrro/luarocks.nvim", priority = 1000, config = true },
-	{ "nvim-neorg/neorg", dependencies = { "luarocks.nvim" }, lazy = false, version = "*", config = true },
+	{
+		"nvim-neorg/neorg",
+		dependencies = { "luarocks.nvim" },
+		lazy = false,
+		version = "*",
+		config = function()
+			require("neorg").setup({
+				["core.defaults"] = {},
+				["core.concealer"] = {},
+				["core.dirman"] = {
+					config = {
+						workspaces = {
+							dev = "~/notes",
+						},
+						index = "index.norg",
+					},
+				},
+			})
+		end,
+	},
 }
