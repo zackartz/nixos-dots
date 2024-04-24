e4cccf7d85c74b2bf1c974fc91b9fe4527d84409
zackartz <zackmyers@lavabit.com>
2024-04-24 11:07:03 -0400

    466 current  2024-04-24 11:07:00  24.05.20240423.0a0027c  6.8.6-zen1                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -35,11 +35,11 @@
     alsa.support32Bit = true;
     pulse.enable = true;
     # If you want to use JACK applications, uncomment this
-    #jack.enable = true;
+    jack.enable = true;
 
     # use the example session manager (no others are packaged yet so this is enabled by default,
     # no need to redefine it in your config for now)
-    #media-session.enable = true;
+    wireplumber.enable = true;
   };
   services.fstrim.enable = true;
 
