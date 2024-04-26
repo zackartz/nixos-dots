0de05774b71ca1ae41a210e562c7dc0a1af4c76c
zackartz <zackmyers@lavabit.com>
2024-04-26 13:47:41 -0400

    473 current  2024-04-26 13:47:33  24.05.20240426.4c86138  6.8.6-zen1                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -44,7 +44,11 @@
   services.fstrim.enable = true;
 
   # Bootloader.
-  boot.loader.systemd-boot.enable = true;
+  boot.loader.systemd-boot.enable = lib.mkForce false;
+  boot.lanzaboote = {
+    enable = true;
+    pkiBundle = "/etc/secureboot";
+  };
   boot.loader.efi.canTouchEfiVariables = true;
 
   networking.hostName = "earth"; # Define your hostname.
