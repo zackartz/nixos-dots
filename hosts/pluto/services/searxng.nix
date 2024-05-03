e558e7d158a029d472c46f6376ea78049b443850
zackartz <zackmyers@lavabit.com>
2024-05-03 15:47:30 -0400

    go back to http

--- hosts/pluto/services/searxng.nix
+++ hosts/pluto/services/searxng.nix
@@ -4,32 +4,20 @@
     package = pkgs.searxng;
     runInUwsgi = true;
     settings = {
+      # server.port = 8080;
+      # server.bind_addres = "0.0.0.0";
       server.secret_key = "6f6bf40218f239718cacbc2cd837792be828e840b48ac72a8e0a9d0ddb9d0b00";
     };
     uwsgiConfig = {
-      socket = "/run/searx/searx.sock";
-      chmod-socket = "660";
+      http = ":8080";
     };
   };
 
   services.nginx.virtualHosts."search.zackmyers.io" = {
     forceSSL = true;
     enableACME = true;
-    locations."/searx".extraConfig = ''
-      uwsgi_pass unix:///run/searx/searx.sock;
-
-      include uwsgi_params;
-
-      uwsgi_param    HTTP_HOST             $host;
-      uwsgi_param    HTTP_CONNECTION       $http_connection;
-
-      # see flaskfix.py
-      uwsgi_param    HTTP_X_SCHEME         $scheme;
-      uwsgi_param    HTTP_X_SCRIPT_NAME    /searxng;
-
-      # see limiter.py
-      uwsgi_param    HTTP_X_REAL_IP        $remote_addr;
-      uwsgi_param    HTTP_X_FORWARDED_FOR  $proxy_add_x_forwarded_for;
-    '';
+    locations."/" = {
+      proxyPass = "http://localhost:8080";
+    };
   };
 }
