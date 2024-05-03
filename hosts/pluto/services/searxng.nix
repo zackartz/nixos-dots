5e27dd218058d8f024659656d436fdb9f9ef033e
zackartz <zackmyers@lavabit.com>
2024-05-03 16:38:20 -0400

    add back baseurl

--- hosts/pluto/services/searxng.nix
+++ hosts/pluto/services/searxng.nix
@@ -7,6 +7,7 @@
       # server.port = 8080;
       # server.bind_addres = "0.0.0.0";
       server.secret_key = "6f6bf40218f239718cacbc2cd837792be828e840b48ac72a8e0a9d0ddb9d0b00";
+      server.base_url = "https://search.zackmyers.io/searx/";
     };
     uwsgiConfig = {
       http = ":8080";
@@ -16,7 +17,7 @@
   services.nginx.virtualHosts."search.zackmyers.io" = {
     forceSSL = true;
     enableACME = true;
-    locations."/" = {
+    locations."/searx/" = {
       proxyPass = "http://localhost:8080";
     };
   };
