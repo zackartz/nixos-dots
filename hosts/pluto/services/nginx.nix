858c4eb9dd5dd693e3398dffc151d5517399f402
zackartz <zackmyers@lavabit.com>
2024-05-05 21:51:47 -0400

    add proxy settings

--- hosts/pluto/services/nginx.nix
+++ hosts/pluto/services/nginx.nix
@@ -2,6 +2,7 @@
   services.nginx = {
     enable = true;
     package = pkgs.nginxStable.override {openssl = pkgs.libressl;};
+    recommendedProxySettings = true;
     virtualHosts = {
       "node.nyc.zackmyers.io" = {
         forceSSL = true;
