f4c325c1294289057536a208d92f0207c385bf4c
zackartz <zackmyers@lavabit.com>
2024-05-06 10:57:33 -0400

    Revert "539 current  2024-05-06 10:56:25  24.05.20240505.25865a4  6.8.6-zen1                          *"
    This reverts commit 6803902f8adf227240f80505a0f1bf4c8b57b966.

--- modules/rice/hyprland/default.nix
+++ modules/rice/hyprland/default.nix
@@ -14,7 +14,7 @@ in {
   imports = [./config.nix ./binds.nix ./rules.nix];
   wayland.windowManager.hyprland = {
     enable = true;
-    package = pkgs.hyprland;
+    package = inputs.hyprland.packages.${pkgs.system}.default;
 
     systemd = {
       variables = ["--all"];
