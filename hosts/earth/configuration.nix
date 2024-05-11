25ac6c2e8cd2fbf95ad06a9da5070bfa2a247f44
zackartz <zackmyers@lavabit.com>
2024-05-11 11:32:54 -0400

    560 current  2024-05-11 11:32:52  24.05.20240509.f1010e0  6.8.6-zen1                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -48,7 +48,7 @@
     mullvad = config.services.mullvad-vpn.package;
   in ''
     while ! ${mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
-    ${mullvad}/bin/mullvad auto-connect set onn
+    ${mullvad}/bin/mullvad auto-connect set on
     ${mullvad}/bin/mullvad tunnel ipv6 set on
     ${mullvad}/bin/mullvad set default \
       --block-ads --block-trackers --block-malware
