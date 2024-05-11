b310598e55b2089931552f2d6eaac75719955f3a
zackartz <zackmyers@lavabit.com>
2024-05-11 12:35:17 -0400

    565 current  2024-05-11 12:35:15  24.05.20240509.f1010e0  6.8.6-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/ui/catppuccin.lua
+++ modules/home-manager/vim/config/lua/plugins/ui/catppuccin.lua
@@ -5,7 +5,7 @@ return {
 		vim.cmd.colorscheme("catppuccin")
 	end,
 	opts = {
-		transparent_background = true,
+		transparent_background = false,
 		custom_highlights = function(colors)
 			return {
 				Pmenu = { bg = colors.base },
