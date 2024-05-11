fd3165b3231330d28136059f5a856c0fc9f93963
zackartz <zackmyers@lavabit.com>
2024-05-11 11:31:29 -0400

    559 current  2024-05-11 11:31:27  24.05.20240509.f1010e0  6.8.6-zen1                          *

--- hosts/earth/configuration.nix
+++ hosts/earth/configuration.nix
@@ -43,6 +43,17 @@
   };
   services.fstrim.enable = true;
   services.mullvad-vpn.enable = true;
+
+  systemd.services."mullvad-daemon".postStart = let
+    mullvad = config.services.mullvad-vpn.package;
+  in ''
+    while ! ${mullvad}/bin/mullvad status >/dev/null; do sleep 1; done
+    ${mullvad}/bin/mullvad auto-connect set onn
+    ${mullvad}/bin/mullvad tunnel ipv6 set on
+    ${mullvad}/bin/mullvad set default \
+      --block-ads --block-trackers --block-malware
+  '';
+
   services.transmission = {
     enable = true;
     package = pkgs.transmission_4;
