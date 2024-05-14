24252cf89b0345888e57a4196563f205193d7806
zackartz <zackmyers@lavabit.com>
2024-05-14 18:10:42 -0400

    603 current  2024-05-14 18:10:38  24.05.20240512.2057814  6.8.6-zen1                          *

--- modules/home-manager/vim/config/lua/plugins/tools/rest.lua
+++ modules/home-manager/vim/config/lua/plugins/tools/rest.lua
@@ -5,11 +5,11 @@ return {
 		config = function()
 			package.path = package.path
 				.. ";"
-				.. "/nix/store/cga26ykqb57jyiws6wvrsmw9xrqf7555-lua5.1-lua-curl-0.3.13-1/share/lua/5.1/?.lua"
+				.. "/nix/store/cga26ykqb57jyiws6wvrsmw9xrqf7555-lua5.1-lua-curl-0.3.13-1/share/lua/5.1/cURL.lua"
 
 			package.cpath = package.cpath
 				.. ";"
-				.. "/nix/store/cga26ykqb57jyiws6wvrsmw9xrqf7555-lua5.1-lua-curl-0.3.13-1/lib/lua/5.1/?.so"
+				.. "/nix/store/cga26ykqb57jyiws6wvrsmw9xrqf7555-lua5.1-lua-curl-0.3.13-1/lib/lua/5.1/lcurl.so"
 
 			require("rest-nvim").setup()
 		end,
