1548230f2a19583511b57debb51bb60f0c3b2121
zackartz <zackmyers@lavabit.com>
2024-05-14 16:42:31 -0400

    588 current  2024-05-14 16:42:28  24.05.20240512.2057814  6.8.6-zen1                          *

--- modules/home-manager/vim/default.nix
+++ modules/home-manager/vim/default.nix
@@ -37,6 +37,7 @@ in {
       fswatch # File watcher utility, replacing libuv.fs_event for neovim 10.0
       sqlite
       postgresql
+      luarocks-nix
       mongosh
       gerbera
       vscode-extensions.vadimcn.vscode-lldb.adapter
