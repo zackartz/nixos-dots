81e21b3b72bf392267fcd1be59c0f6acf616f64b
zackartz <zackmyers@lavabit.com>
2024-05-14 15:54:58 -0400

    586 current  2024-05-14 15:54:55  24.05.20240512.2057814  6.8.6-zen1                          *

---
+++ modules/home-manager/vim/config/lua/plugins/tools/rest.lua
@@ -0,0 +1,15 @@
+return {
+	"vhyrro/luarocks.nvim",
+	priority = 1000,
+	config = true,
+	opts = {
+		rocks = { "lua-curl", "nvim-nio", "mimetypes", "xml2lua" },
+	},
+}, {
+	"rest-nvim/rest.nvim",
+	ft = "http",
+	dependencies = { "luarocks.nvim" },
+	config = function()
+		require("rest-nvim").setup()
+	end,
+}
