ecfe196f8bab84216290cfd7c0ad9e01583b8213
zackartz <zackmyers@lavabit.com>
2024-05-14 17:04:00 -0400

    592 current  2024-05-14 17:03:57  24.05.20240512.2057814  6.8.6-zen1                          *

--- modules/home-manager/vim/default.nix
+++ modules/home-manager/vim/default.nix
@@ -1,6 +1,7 @@
 {
   config,
   pkgs,
+  lib,
   ...
 }: let
   nvimDir = "/home/${config.home.username}/nixos/modules/home-manager/vim";
@@ -58,23 +59,23 @@ in {
     };
   };
 
-  # home.activation.neovim = hm.dag.entryAfter ["linkGeneration"] ''
-  #   #! /bin/bash
-  #   NVIM_WRAPPER=~/.nix-profile/bin/nvim
-  #   STATE_DIR=~/.local/state/nix/
-  #   STATE_FILE=$STATE_DIR/lazy-lock-checksum
-  #   LOCK_FILE=~/.config/nvim/lazy-lock.json
-  #   HASH=$(nix-hash --flat $LOCK_FILE)
-  #
-  #   [ ! -d $STATE_DIR ] && mkdir -p $STATE_DIR
-  #   [ ! -f $STATE_FILE ] && touch $STATE_FILE
-  #
-  #   if [ "$(cat $STATE_FILE)" != "$HASH" ]; then
-  #     echo "Syncing neovim plugins"
-  #     PATH="$PATH:${pkgs.git}/bin" $DRY_RUN_CMD $NVIM_WRAPPER --headless "+Lazy! restore" +qa
-  #     $DRY_RUN_CMD echo $HASH >$STATE_FILE
-  #   else
-  #     $VERBOSE_ECHO "Neovim plugins already synced, skipping"
-  #   fi
-  # '';
+  home.activation.neovim = lib.hm.dag.entryAfter ["linkGeneration"] ''
+    #! /bin/bash
+    NVIM_WRAPPER=~/.nix-profile/bin/nvim
+    STATE_DIR=~/.local/state/nix/
+    STATE_FILE=$STATE_DIR/lazy-lock-checksum
+    LOCK_FILE=~/.config/nvim/lazy-lock.json
+    HASH=$(nix-hash --flat $LOCK_FILE)
+
+    [ ! -d $STATE_DIR ] && mkdir -p $STATE_DIR
+    [ ! -f $STATE_FILE ] && touch $STATE_FILE
+
+    if [ "$(cat $STATE_FILE)" != "$HASH" ]; then
+      echo "Syncing neovim plugins"
+      PATH="$PATH:${pkgs.git}/bin" $DRY_RUN_CMD $NVIM_WRAPPER --headless "+Lazy! restore" +qa
+      $DRY_RUN_CMD echo $HASH >$STATE_FILE
+    else
+      $VERBOSE_ECHO "Neovim plugins already synced, skipping"
+    fi
+  '';
 }
