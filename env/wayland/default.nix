6d6eb6b31f45dc20886595ca8e92dfca6169b203
zackartz <zackmyers@lavabit.com>
2024-05-21 12:16:20 -0400

    660 current  2024-05-21 12:16:13  24.05.20240521.d3cda6a  6.8.6-zen1                          *

--- env/wayland/default.nix
+++ env/wayland/default.nix
@@ -39,7 +39,7 @@
       DISABLE_QT_COMPAT = "0";
       QT_WAYLAND_DISABLE_WINDOWDECORATION = "1";
       MOZ_ENABLE_WAYLAND = "1";
-      WLR_BACKEND = "vulkan";
+      WLR_BACKEND = "wayland";
       WLR_RENDERER = "vulkan";
       XDG_SESSION_TYPE = "wayland";
       SDL_VIDEODRIVER = "wayland";
