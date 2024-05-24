f16e0f463b6e071be25862bc01760da9908c2e69
zackartz <zackmyers@lavabit.com>
2024-05-24 11:56:23 -0400

    disable promtail

--- modules/nixos/sites/grafana/default.nix
+++ modules/nixos/sites/grafana/default.nix
@@ -136,42 +136,42 @@ in {
     #   };
     # };
 
-    services.promtail = {
-      enable = true;
-      configuration = {
-        server = {
-          http_listen_port = 3031;
-          grpc_listen_port = 0;
-        };
-        positions = {
-          filename = "/tmp/positions.yaml";
-        };
-        clients = [
-          {
-            url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
-          }
-        ];
-        scrape_configs = [
-          {
-            job_name = "journal";
-            journal = {
-              max_age = "12h";
-              labels = {
-                job = "systemd-journal";
-                host = "pluto";
-              };
-            };
-            relabel_configs = [
-              {
-                source_labels = ["__journal__systemd_unit"];
-                target_label = "unit";
-              }
-            ];
-          }
-        ];
-      };
-      # extraFlags
-    };
+    # services.promtail = {
+    #   enable = true;
+    #   configuration = {
+    #     server = {
+    #       http_listen_port = 3031;
+    #       grpc_listen_port = 0;
+    #     };
+    #     positions = {
+    #       filename = "/tmp/positions.yaml";
+    #     };
+    #     clients = [
+    #       {
+    #         url = "http://127.0.0.1:${toString config.services.loki.configuration.server.http_listen_port}/loki/api/v1/push";
+    #       }
+    #     ];
+    #     scrape_configs = [
+    #       {
+    #         job_name = "journal";
+    #         journal = {
+    #           max_age = "12h";
+    #           labels = {
+    #             job = "systemd-journal";
+    #             host = "pluto";
+    #           };
+    #         };
+    #         relabel_configs = [
+    #           {
+    #             source_labels = ["__journal__systemd_unit"];
+    #             target_label = "unit";
+    #           }
+    #         ];
+    #       }
+    #     ];
+    #   };
+    #   # extraFlags
+    # };
 
     services.nginx.virtualHosts.${config.services.grafana.domain} = {
       forceSSL = true;
