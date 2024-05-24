92d46c6a4cb01b05d8a7123ee615f8807cc0685d
zackartz <zackmyers@lavabit.com>
2024-05-24 11:53:35 -0400

    fix loki config

--- modules/nixos/sites/grafana/default.nix
+++ modules/nixos/sites/grafana/default.nix
@@ -86,7 +86,6 @@ in {
           max_chunk_age = "1h";
           chunk_target_size = 999999;
           chunk_retain_period = "30s";
-          max_transfer_retries = 0;
         };
 
         schema_config = {
@@ -109,7 +108,6 @@ in {
             active_index_directory = "/var/lib/loki/boltdb-shipper-active";
             cache_location = "/var/lib/loki/boltdb-shipper-cache";
             cache_ttl = "24h";
-            shared_store = "filesystem";
           };
 
           filesystem = {
@@ -122,10 +120,6 @@ in {
           reject_old_samples_max_age = "168h";
         };
 
-        chunk_store_config = {
-          max_look_back_period = "0s";
-        };
-
         table_manager = {
           retention_deletes_enabled = false;
           retention_period = "0s";
@@ -133,7 +127,6 @@ in {
 
         compactor = {
           working_directory = "/var/lib/loki";
-          shared_store = "filesystem";
           compactor_ring = {
             kvstore = {
               store = "inmemory";
