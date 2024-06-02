c2303c0a398c5322e5f3180300a9516f554a173a
zackartz <zackmyers@lavabit.com>
2024-06-02 11:02:44 -0400

    727 current  2024-06-02 11:02:41  24.11.20240529.ad57eef  6.9.2-zen1                                            *

--- modules/nixos/services/gh/default.nix
+++ modules/nixos/services/gh/default.nix
@@ -9,7 +9,7 @@ with lib.custom; let
   cfg = config.services.gh;
 
   sec = config.age.secrets;
-  group = config.services.github-runner.pluto.runnerGroup;
+  group = config.services.github-runners.pluto.runnerGroup;
 in {
   options.services.gh = with types; {
     enable = mkBoolOpt false "Enable GitHub Actions Runner";
@@ -19,11 +19,10 @@ in {
     age.secrets = {
       github_runner = {
         file = ./sec/github_runner.age;
-        group = group;
       };
     };
 
-    services.github-runner.pluto = {
+    services.github-runners.pluto = {
       enable = true;
       url = "https://github.com/zackartz/nixos-dots";
       tokenFile = sec.github_runner.path;
