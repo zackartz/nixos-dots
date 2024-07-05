03c2cdadfb7c286d18bc549e53f95ef982576593
zackartz <zackmyers@lavabit.com>
2024-07-05 10:30:46 -0400

    810 current  2024-07-05 10:30:41  24.11.20240705.d9c0b9d  6.9.7-zen1                                            *

---
+++ modules/home/apps/tools/neovim/config/lua/plugins/editor/markview.lua
@@ -0,0 +1,11 @@
+return {
+	"OXY2DEV/markview.nvim",
+
+	dependencies = {
+		-- You may not need this if you don't lazy load
+		-- Or if the parsers are in your $RUNTIMEPATH
+		"nvim-treesitter/nvim-treesitter",
+
+		"nvim-tree/nvim-web-devicons",
+	},
+}
