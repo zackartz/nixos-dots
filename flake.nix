2a3756e73544788ef2e2df3e8aabc417cfce73cd
zack <hi@zoeys.computer>
2024-07-11 14:26:40 -0400

    823 current  2024-07-11 14:26:34  24.11.20240710.5f3d1d6  6.9.8-zen1                                            *

--- flake.nix
+++ flake.nix
@@ -11,7 +11,7 @@
   };
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable-small";
 
     home-manager = {
       url = "github:nix-community/home-manager";
