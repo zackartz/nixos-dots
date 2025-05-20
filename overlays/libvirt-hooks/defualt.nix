916f497b84afedcccceed7cd390e8bff74d6f728
zack <hi@zoeys.computer>
2025-05-20 11:09:14 -0400

    cleanup

--- overlays/libvirt-hooks/defualt.nix
+++ overlays/libvirt-hooks/defualt.nix
@@ -1,5 +1,4 @@
 self: super: {
-  # We are overriding the libvirtd entry in nixosModules
   nixosModules =
     super.nixosModules
     // {
@@ -9,15 +8,11 @@ self: super: {
         pkgs,
         ...
       } @ args: let
-        # Import and evaluate the original libvirtd module definition
-        # super.nixosModules.libvirtd is typically the path to the original module file
         originalModule = import super.nixosModules.libvirtd args;
       in
         lib.mkMerge [
-          # Include everything from the original module (its options, config assignments, etc.)
           originalModule
 
-          # Add our overriding configuration
           {
             config = lib.mkIf config.virtualisation.libvirtd.enable {
               virtualisation.libvirtd.hooks = lib.mkForce {};
